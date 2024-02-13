b2bc05b2bd7ed07ebcfad5610922d26cc87d923f
Saibalajinikki <saibalaji.nikki@gmail.com>
2024-02-13 09:30:07 -0500

    The web app is ready

--- app.py
+++ app.py
@@ -23,5 +23,13 @@ def predict_api():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+@app.route('/predict', methods =['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = np.array(data).reshape(1,-1)
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template('home.html', prediction_test ='The predicted House value is {}'.format(output))
+
 if __name__ == '__main__':
     app.run(debug=True)
