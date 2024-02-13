f22d7fe2c00f478a270a01e4043b954c0db630a1
Saibalajinikki <saibalaji.nikki@gmail.com>
2024-02-13 10:41:51 -0500

    modified app.py

--- app.py
+++ app.py
@@ -16,28 +16,35 @@ def predict_api():
         data = request.json
         if data is not None and 'data' in data:
             data = data['data']
-            print("Raw data from JSON:", data)
+            app.logger.info("Raw data from JSON: %s", data)
 
             input_array = np.array(list(data.values())).reshape(1, -1)
-            print("Input array for prediction:", input_array)
+            app.logger.info("Input array for prediction: %s", input_array)
 
             output = regmodel.predict(input_array)
-            print("Prediction output:", output[0])
+            app.logger.info("Prediction output: %s", output[0])
 
             return jsonify({'prediction': output[0]})
         else:
             return jsonify({'error': 'Invalid JSON format. Expected {"data": {...}}'})
     except Exception as e:
+        app.logger.error("Error during prediction: %s", str(e))
         return jsonify({'error': str(e)})
 
-
-@app.route('/predict', methods =['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data = [float(x) for x in request.form.values()]
-    final_input = np.array(data).reshape(1,-1)
-    print(final_input)
-    output = regmodel.predict(final_input)[0]
-    return render_template('home.html', prediction_test ='The predicted House value is {}'.format(output))
+    try:
+        data = [float(x) for x in request.form.values()]
+        final_input = np.array(data).reshape(1, -1)
+        app.logger.info("Input array for prediction: %s", final_input)
+
+        output = regmodel.predict(final_input)[0]
+        app.logger.info("Prediction output: %s", output)
+
+        return render_template('home.html', prediction_test='The predicted House value is {}'.format(output))
+    except Exception as e:
+        app.logger.error("Error during prediction: %s", str(e))
+        return render_template('home.html', prediction_test='Error during prediction: {}'.format(str(e)))
 
 if __name__ == '__main__':
     app.run(debug=True)
